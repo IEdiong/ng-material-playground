ac83e694b7488334cda9df314da63b13b75fa3f6
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-07-08 04:20:38 +0100

    add sidenav component

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -3,9 +3,19 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [],
-  exports: [MatButtonModule, MatIconModule, MatCheckboxModule],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatSidenavModule,
+  ],
 })
 export class MaterialModule {}
