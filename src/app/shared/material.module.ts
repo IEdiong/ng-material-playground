37307b06d14d22b2e7c5de57feea62901f8b1056
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-07-08 08:03:56 +0100

    setup layout for the contact-manager app

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -8,6 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [],
@@ -20,6 +21,7 @@ import { MatListModule } from '@angular/material/list';
     MatSidenavModule,
     MatToolbarModule,
     MatListModule,
+    MatCardModule,
   ],
 })
 export class MaterialModule {}
