dbe01054291903f9c9a35f03b2bb02b8f4a340d3
Ediongsenyene Joseph <ediongi6@hotmail.com>
2023-09-01 11:34:00 +0100

    implement apex-chart

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -10,6 +10,8 @@ import { FlexboxComponent } from './flexbox/flexbox.component';
 import { DemoComponent } from './demo.component';
 import { UserComponent } from './user/user.component';
 import { AssessmentComponent } from './assessment/assessment.component';
+import { ChartsComponent } from './charts/charts.component';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 const routes: Routes = [
   {
@@ -18,7 +20,7 @@ const routes: Routes = [
     children: [
       { path: 'user', component: UserComponent },
       { path: 'assessment', component: AssessmentComponent },
-      { path: '', component: UserComponent },
+      { path: '', component: ChartsComponent },
     ],
   },
 ];
@@ -30,12 +32,14 @@ const routes: Routes = [
     DemoComponent,
     UserComponent,
     AssessmentComponent,
+    ChartsComponent,
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     MaterialModule,
     FormsModule,
+    NgApexchartsModule,
   ],
   exports: [RouterModule],
 })
